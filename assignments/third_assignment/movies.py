a8bef072191a51b4be8cb3db4e88c39586a52ca4
Adam Wistoft <44964494+Wistoft2410@users.noreply.github.com>
2021-10-10 23:06:29 +0200

    codingbat added and movies.py done

--- assignments/third_assignment/movies.py
+++ assignments/third_assignment/movies.py
@@ -5,22 +5,29 @@ def read_csv(filename):
 
     for line in lines:
         new_list.append(tuple(line.strip("\n").split(",")))
+
     return new_list
 
 def generate_name_map(nodes):
+
     node_dict = {}
+
     for node in nodes:
         node_dict.setdefault(node[0],node[1])
+
     return node_dict
 
 def name_edges(edges,name_map):
+
     new_list = []
+
     for edge in edges:
         new_list.append((name_map[edge[0]], name_map[edge[1]], edge[2]))
 
     return new_list
 
 def generate_movie_dictionary(named_edges):
+
     movie_dict = {}
 
     for edge in named_edges: 
@@ -32,7 +39,9 @@ def generate_movie_dictionary(named_edges):
     return movie_dict
 
 def get_actor_friends(movie_dictionary):
+
     friends_dict = {}
+
     for movie in movie_dictionary: 
         for actor in movie_dictionary[movie]: 
             friends_dict.setdefault(actor, [])
@@ -40,22 +49,26 @@ def get_actor_friends(movie_dictionary):
                 if o_actor != actor and not o_actor in friends_dict[actor]:
                     friends_dict[actor].append(o_actor)
 
-        return friends_dict
+    return friends_dict
 
 
 
 def main():
-    # assigning the csv files
+    # assigning variables with csv files
     edges = "/Users/computer/Documents/GitHub/python_examples/assignments/third_assignment/data/edges.csv"
     nodes = "/Users/computer/Documents/GitHub/python_examples/assignments/third_assignment/data/nodes.csv"
+    
+    # reading csv files feat. lil-read_csv
     node = read_csv(nodes)
     edge = read_csv(edges)
-    g_n_m = generate_name_map(node)
-    n_e = name_edges(edge,g_n_m)
-    movie = generate_movie_dictionary(n_e)
+    
+    # using csv files in functions 
+    gen_nm = generate_name_map(node)
+    name_e = name_edges(edge,gen_nm)
+    movie = generate_movie_dictionary(name_e)
     actor_friends = get_actor_friends(movie)
 
-    print(actor_friends["Leonardo DiCaprio"])
+    print(get_actor_friends("Leonardo DiCaprio"))
 
 if __name__ == "__main__":
     main()
